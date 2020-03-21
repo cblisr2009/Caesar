a8ffd974983be3420617a38365f4d85134854f22
cblisr2009 <60400919+cblisr2009@users.noreply.github.com>
2020-03-20 23:47:53 -0500

    Update caesar.cpp

--- caesar.cpp
+++ caesar.cpp
@@ -10,15 +10,26 @@
 #include "tinyxml2.h"
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace tinyxml2;
 using namespace std;
+struct Document
+{
 
-int main(int argc, const char ** argv)
+std::vector<std::string> speakers;
+std::vector<std::string> lines;
+};
+static std:: string getElementText (XMLElement * _element);
+static std::vector<std::string> getTextlist(XMLElement * _element);
+
+int main(int argc, const char * argv[])
 
 {
+
     XMLDocument *doc = new XMLDocument();
 doc->LoadFile("j_caesar.xml");
+    
 //
 //    This runs and prints nothing
 //    for(tinyxml2::XMLElement* child = doc->FirstChildElement("PLAY")->FirstChildElement("TITLE");
@@ -43,10 +54,20 @@ if(doc->ErrorID() == 0)
     doc->FirstChildElement( "PLAY" )->FirstChildElement("TITLE"  )->GetText()<<std::endl;
 //successfully prints the title of the play "The Tragedy of Julius Caesar"f
      
-        XMLElement * pElement = doc->FirstChildElement("PLAY");
-//
-//
-       std::cout<< pElement->FirstChildElement("ACT")->FirstChildElement("TITLE")->GetText()<<std::endl;
+        XMLElement * pRoot = doc->FirstChildElement("PLAY");
+//        XMLElement * actElement = doc->FirstChildElement("PLAY")->FirstChildElement("ACT")->FirstChildElement("TITLE");
+//        const char* act = actElement->GetText();
+               
+//To print Act of the play
+       std::cout<< pRoot->FirstChildElement("ACT")->FirstChildElement("TITLE")->GetText()<<std::endl;
+    
+    //To print the Scene of the play
+        std::cout<<
+        pRoot->FirstChildElement("ACT")->FirstChildElement("SCENE")->FirstChildElement("TITLE")->GetText()<<std::endl;
+        
+       std::cout<<
+        pRoot->FirstChildElement("ACT")->FirstChildElement("SCENE")->FirstChildElement("TITLE")->NextSiblingElement("SPEECH")->GetText()<<std::endl;
+        
 //    }
        
 //then when trying to reach further, not necessarily always this way but i will always get the Title and then I will get segmentation fault
